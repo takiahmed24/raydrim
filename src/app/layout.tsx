01c9d5495c0fff3a61eeb10c3a5de43e0b983761
takiahmed24 <muhammadtakiahmed@gmail.com>
2026-07-30 13:59:21 +0600

    Add Google AdSense script to layout head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,6 +92,11 @@ export default function RootLayout({
     <html lang="en" className={`${outfit.variable} ${inter.variable}`}>
       <head>
         <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4263728957042690"
+          crossOrigin="anonymous"
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(jsonLdOrganization),
